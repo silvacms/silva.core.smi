598667856d7be004dda96d8919279f77c8b889ad
Andy Altepeter <aaltepet@altepeter.net>
2008-11-17 04:01:06 +0000

    add kupu and forms editor buttons to IDocuments

--- silva/core/smi/edit.py
+++ silva/core/smi/edit.py
@@ -6,7 +6,9 @@ from silva.core.smi import properties
 from silva.core.smi import smi
 from silva.core import conf as silvaconf
 
+from Products.Silva.i18n import translate as _
 from Products.Silva import interfaces
+from Products.SilvaDocument.interfaces import IDocument
 
 silvaconf.templatedir('templates')
 silvaconf.view(smi.EditTab)
@@ -14,3 +16,23 @@ silvaconf.view(smi.EditTab)
 class PublishNowButton(properties.PublishNowButton):
 
     silvaconf.context(interfaces.IVersionedContent)
+
+class KupuEditorButton(smi.SMIButton):
+    silvaconf.context(IDocument)
+    silvaconf.order(10)
+
+    tab = 'tab_edit?editor=kupu'
+    label = _(u"kupu editor")
+    help = _(u"edit with the kupu editor: alt-(")
+    accesskey = '('
+
+class FormsEditorButton(smi.SMIButton):
+    silvaconf.context(IDocument)
+    silvaconf.order(10)
+
+    tab = 'tab_edit?editor=forms_editor'
+    label = _(u"forms editor...")
+    help = _(u"edit with the forms editor: alt-)")
+    accesskey = ')'
+
+    
