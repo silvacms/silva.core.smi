f77702161e28f3bde9c1c58158a67c31720c8581
Sylvain Viollon <thefunny@gmail.com>
2009-09-17 17:35:10 +0000

    We use megrok.layout now.

--- silva/core/smi/error.py
+++ silva/core/smi/error.py
@@ -8,36 +8,21 @@ from zope.security.interfaces import IUnauthorized
 from zope.interface import implements
 
 from silva.core.layout.interfaces import ISMILayer
-from silva.core.views.interfaces import ITemplate
 from silva.core.views import views as silvaviews
 from silva.core import conf as silvaconf
 
 # 404 page
 
-class IErrorPage(ITemplate):
-    pass
-
-class ErrorPage(silvaviews.Template):
+class ErrorPage(silvaviews.Page):
     silvaconf.context(INotFound)
     silvaconf.name('error.html')
     silvaconf.layer(ISMILayer)
 
-    implements(IErrorPage)
-
-    def __call__(self):
-        # FIXME: Why do I get a redirect and notfound in /edit ?
-        self.update()
-        return self.template.render(self)
 
 # Unauthorized page
 
-class IUnauthorizedPage(ITemplate):
-    pass
-
-class UnauthorizedPage(silvaviews.Template):
+class UnauthorizedPage(silvaviews.Page):
     silvaconf.context(IUnauthorized)
     silvaconf.name('error.html')
     silvaconf.layer(ISMILayer)
-
-    implements(IUnauthorizedPage)
 
