c0f51476ae5d3d87262881a44b278bc90700edf4
Sylvain Viollon <thefunny@gmail.com>
2010-07-06 17:06:13 +0000

    Add a another error page for export.

---
+++ src/silva/core/smi/errors/export.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2008-2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from five import grok
+
+from silva.core.views import views as silvaviews
+from Products.Silva.silvaxml.xmlexport import ExternalReferenceError
+
+
+class ExternalReferenceErrorPage(silvaviews.Page):
+    """ Page to render broken references errors.
+
+    It redirects to break references form if the user as the necessary rights to
+    break references.
+    """
+    grok.context(ExternalReferenceError)
+    grok.name('error.html')
+
+    def update(self):
+        origin, target, exported = self.context.error.args
+        self.origin = origin.get_silva_object()
+        self.target = target.get_silva_object()
+        self.exported = exported.get_silva_object()
