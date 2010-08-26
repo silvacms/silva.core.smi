0d25485d6244bbe6ab5971948cb15c6cfbccc229
Sylvain Viollon <thefunny@gmail.com>
2010-08-26 09:07:31 +0000

    Focus the first required field of a form.

--- src/silva/core/smi/static/smi.js
+++ src/silva/core/smi/static/smi.js
@@ -1,7 +1,9 @@
 
-// Add new content
 $(document).ready(function() {
+    // Add new content
     $('#md-container-field-content').change(function() {
         $('#md-container-action-new').trigger('click');
     });
+    // Focus first required field
+    $('.zeam-form .field-required:first').trigger('focus');
 });
