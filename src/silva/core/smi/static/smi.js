160b6b4023fd8f670e21bd57d6ef5cc1d8b9c073
Sylvain Viollon <thefunny@gmail.com>
2010-09-03 10:17:30 +0000

    Fill the add position field if possible.

--- src/silva/core/smi/static/smi.js
+++ src/silva/core/smi/static/smi.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
 
     // Add new content auto submit
     $('#md-container-field-content').bind('change', function() {
+        // Fill in the add in position field if possible
+        var form = $('form[name=silvaObjects]');
+        if (form) {
+            var position = form.find('input[name=ids:list]:checked');
+            if (position.length == 1) {
+                var label = $('label[for=' + position.attr('id') + ']').text();
+                $('#md-container-field-position').val(
+                    label.replace(/^[^\d]*(\d*).*$/g, '$1'));
+            };
+        }
         $('#md-container-action-new').trigger('click');
     });
     $(document).bind('zeam-popup-closed', function() {
