6c88a7a1782783936bead808a83f81e238e19bc4
Sylvain Viollon <thefunny@gmail.com>
2010-09-16 12:50:03 +0000

    Add a work error page, you are over quota.

---
+++ src/silva/core/smi/errors/quota.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from five import grok
+from silva.core.views import views as silvaviews
+from zope.traversing.browser import absoluteURL
+
+from Acquisition import aq_parent
+from Products.Silva.mangle import Bytes
+from Products.Silva.Publication import OverQuotaException
+
+
+class OverQuotaErrorPage(silvaviews.Page):
+    """ Page to render broken references errors.
+
+    It redirects to break references form if the user as the necessary rights to
+    break references.
+    """
+    grok.context(OverQuotaException)
+    grok.name('error.html')
+    grok.template('error')
+
+    def update(self):
+        self.exceeding_size = Bytes(self.context.error.args[0])
+        self.next_url = absoluteURL(aq_parent(self.context), self.request) + '/edit'
