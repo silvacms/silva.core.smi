90f176110b0294b7b7bd48e41259ad8f3e655339
Sylvain Viollon <thefunny@gmail.com>
2011-03-29 15:16:39 +0200

    API update.

--- src/silva/core/smi/content/publish.py
+++ src/silva/core/smi/content/publish.py
@@ -8,7 +8,9 @@ from datetime import datetime
 from AccessControl.security import checkPermission
 
 from Products.Silva.Versioning import VersioningError
-from silva.core import interfaces as silvainterfaces
+from silva.core.interfaces import IVersion, IVersionManager
+from silva.core.interfaces import IVersioning, IVersionedContent
+from silva.core.interfaces import IPublicationWorkflow, PublicationWorkflowError
 from silva.core.smi.widgets.zeamform import PublicationStatus
 from silva.core.views import views as silvaviews
 from silva.translations import translate as _
@@ -29,7 +31,7 @@ from zeam.form.ztk.actions import EditAction
 class Publish(silvaforms.SMIComposedForm):
     """Publish tab.
     """
-    grok.adapts(Screen, silvainterfaces.IVersionedContent)
+    grok.adapts(Screen, IVersionedContent)
     grok.require('silva.ChangeSilvaContent')
     grok.name('publish')
 
@@ -37,7 +39,7 @@ class Publish(silvaforms.SMIComposedForm):
 
 
 class PublishMenu(MenuItem):
-    grok.adapts(ContentMenu, silvainterfaces.IVersionedContent)
+    grok.adapts(ContentMenu, IVersionedContent)
     grok.require('silva.ChangeSilvaContent')
     grok.order(30)
     name = _('Publish')
@@ -46,7 +48,7 @@ class PublishMenu(MenuItem):
 
 
 class PublicationInfo(silvaviews.Viewlet):
-    grok.context(silvainterfaces.IVersionedContent)
+    grok.context(IVersionedContent)
     grok.view(Publish)
     grok.viewletmanager(silvaforms.SMIFormPortlets)
 
@@ -82,7 +84,7 @@ class IPublicationMessage(Interface):
 
 
 class VersionPublication(grok.Adapter):
-    grok.context(silvainterfaces.IVersioning)
+    grok.context(IVersioning)
     grok.implements(IPublicationInformation)
 
     def get_expiration_datetime(self):
@@ -106,7 +108,7 @@ class VersionPublication(grok.Adapter):
 
 
 class MessagePublication(object):
-    grok.context(silvainterfaces.IVersioning)
+    grok.context(IVersioning)
     grok.implements(IPublicationMessage)
 
     def get_message(self):
@@ -141,9 +143,9 @@ class RequestApprovalAction(PublicationAction):
     def execute(self, form, content, data):
         message = data.getWithDefault('message')
         try:
-            silvainterfaces.IPublicationWorkflow(
+            IPublicationWorkflow(
                 content).request_approval(message)
-        except silvainterfaces.PublicationWorkflowError as e:
+        except PublicationWorkflowError as e:
             form.send_message(e.message, type='error')
             return silvaforms.FAILURE
         form.send_message(_("Approval requested."), type='feedback')
@@ -151,7 +153,7 @@ class RequestApprovalAction(PublicationAction):
 
 
 class RequestApprovalForm(silvaforms.SMISubForm):
-    grok.context(silvainterfaces.IVersionedContent)
+    grok.context(IVersionedContent)
     grok.view(Publish)
     grok.order(20)
     fields = autofields.FieldsCollector(IPublicationFields)
@@ -180,8 +182,8 @@ class PublishAction(PublicationAction):
 
     def execute(self, form, content, data):
         try:
-            silvainterfaces.IPublicationWorkflow(content).approve()
-        except silvainterfaces.PublicationWorkflowError as e:
+            IPublicationWorkflow(content).approve()
+        except PublicationWorkflowError as e:
             form.send_message(e.message, type='error')
             return silvaforms.FAILURE
         form.send_message(_("approved."), type='feedback')
@@ -200,8 +202,8 @@ class ApproveAction(PublicationAction):
     def execute(self, form, content, data):
         try:
             # XXX approve here should take the publication datetime but doesnt !
-            silvainterfaces.IPublicationWorkflow(content).approve()
-        except silvainterfaces.PublicationWorkflowError as e:
+            IPublicationWorkflow(content).approve()
+        except PublicationWorkflowError as e:
             form.send_message(e.message, type='error')
             return silvaforms.FAILURE
         form.send_message(_("approved."), type='feedback')
@@ -209,7 +211,7 @@ class ApproveAction(PublicationAction):
 
 
 class PublicationForm(silvaforms.SMISubForm):
-    grok.context(silvainterfaces.IVersionedContent)
+    grok.context(IVersionedContent)
     grok.view(Publish)
     grok.order(20)
     fields = autofields.FieldsCollector(IPublicationFields)
@@ -241,7 +243,7 @@ class RequestApprovalMessage(autofields.AutoFields):
 
 class PendingApprovalRequestForm(silvaforms.SMISubForm):
     grok.baseclass()
-    grok.context(silvainterfaces.IVersionedContent)
+    grok.context(IVersionedContent)
     grok.view(Publish)
     grok.order(20)
 
@@ -277,8 +279,8 @@ class WithdrawApprovalRequestForm(PendingApprovalRequestForm):
             return silvaforms.FAILURE
         message = data.getWithDefault('message')
         try:
-            silvainterfaces.IPublicationWorkflow(self.context).withdraw_request(message)
-        except silvainterfaces.PublicationWorkflowError as e:
+            IPublicationWorkflow(self.context).withdraw_request(message)
+        except PublicationWorkflowError as e:
             self.send_message(e.message, type='error')
             return silvaforms.FAILURE
         self.send_message('Withdrew request for approval', type='feedback')
@@ -310,9 +312,8 @@ class RejectApprovalRequestForm(PendingApprovalRequestForm):
             return silvaforms.FAILURE
         message = data.getWithDefault('message')
         try:
-            silvainterfaces.IPublicationWorkflow(
-                self.context).reject_request(message)
-        except silvainterfaces.PublicationWorkflowError as e:
+            IPublicationWorkflow(self.context).reject_request(message)
+        except PublicationWorkflowError as e:
             self.send_message(e.message, type='error')
             return silvaforms.FAILURE
         self.send_message('Rejected request for approval', type='feedback')
@@ -322,7 +323,7 @@ class RejectApprovalRequestForm(PendingApprovalRequestForm):
 class ManualCloseForm(silvaforms.SMISubForm):
     """ Close the public version.
     """
-    grok.context(silvainterfaces.IVersionedContent)
+    grok.context(IVersionedContent)
     grok.view(Publish)
     grok.order(30)
 
@@ -340,17 +341,18 @@ class ManualCloseForm(silvaforms.SMISubForm):
         implements=IRemoverAction)
     def close(self):
         try:
-            silvainterfaces.IPublicationWorkflow(self.context).close()
-        except silvainterfaces.PublicationWorkflowError as e:
+            IPublicationWorkflow(self.context).close()
+        except PublicationWorkflowError as e:
             self.send_message(e.message, type='error')
             return silvaforms.FAILURE
 
 
 # sort the versions, order approved, unapproved, published, closed
 def sort_versions(a, b):
-    ast = a.version_status()
-    bst = b.version_status()
-    order = ['unapproved', 'approved', 'public', 'last_closed', 'closed']
+    # XXX This is fast. awsome.
+    ast = IVersionManager(a).get_status()
+    bst = IVersionManager(b).get_status()
+    order = ['unapproved', 'approved', 'published', 'last_closed', 'closed']
     ret = cmp(order.index(ast), order.index(bst))
     if ret == 0:
         try:
@@ -370,48 +372,46 @@ class IPublicationStatusInfo(Interface):
 
 
 class PublicationStatusInfo(grok.Adapter):
-    grok.context(silvainterfaces.IVersion)
+    grok.context(IVersion)
     grok.provides(IPublicationStatusInfo)
 
     def __init__(self, context):
         self.context = context
-        self.versioned_content = self.context.get_content()
-        self.version_manager = silvainterfaces.IVersionManagement(
-            self.context.get_content())
+        self.manager = IVersionManager(self.context)
 
     @property
     def modification_time(self):
-        dt = self.version_manager.getVersionModificationTime(self.context.id)
+        dt = self.manager.get_modification_datetime()
         if dt is not None:
             return dt.asdatetime()
 
     @property
     def publication_time(self):
-        dt = self.version_manager.getVersionPublicationTime(self.context.id)
+        dt = self.manager.get_publication_datetime()
         if dt is not None:
             return dt.asdatetime()
 
     @property
     def expiration_time(self):
-        dt = self.version_manager.getVersionExpirationTime(self.context.id).asdatetime()
+        dt = self.manager.get_expiration_datetime()
         if dt is not None:
             return dt.asdatetime()
 
     @property
     def last_author(self):
-        author = self.version_manager.getVersionLastAuthorInfo(self.context.id)
+        author = self.manager.get_last_author()
         if author is not None:
             return author.fullname()
 
     @property
     def version_status(self):
-        return self.version_manager.getVersionStatus(self.context.id)
+        return self.manager.get_status()
 
-    def delete_version(self):
-        return self.version_manager.deleteVersions([self.context.id])
+    def delete(self):
+        return self.manager.delete()
 
-    def copy_version_for_editing(self):
-        return self.version_manager.revertPreviousToEditable(self.context.id)
+    def copy_for_editing(self):
+        return self.manager.make_editable()
 
 # XXX : TODO...
 # class Compare(silvaforms.Action):
@@ -426,12 +426,12 @@ class CopyForEditingAction(silvaforms.Action):
                     'the selected old one')
     def __call__(self, form, content, line):
         try:
-            content.copy_version_for_editing()
+            content.copy_for_editing()
             form.send_message(
                 _("Reverted to previous version."), type='feedback')
             return silvaforms.SUCCESS
         except VersioningError as e:
-            form.send_message(unicode(e), type='error')
+            form.send_message(e.reason(), type='error')
             return silvaforms.FAILURE
 
 
@@ -444,27 +444,20 @@ class DeleteVersionAction(silvaforms.Action):
 
     def __call__(self, form, content, line):
         try:
-            status = silvaforms.SUCCESS
-            for id, error in content.delete_version():
-                if error:
-                    form.send_message(
-                        _('could not delete ${id}: ${error}',
-                            mapping={'id': id, 'error': error}),
-                        type='error')
-                    status = silvaforms.FAILURE
-                else:
-                    form.send_message(_('deleted ${id}',
-                        mapping={'id': id}), type='feedback')
-            return status
+            content.delete()
+            form.send_message(
+                _('Deleted version'),
+                type='feedback')
+            return silvaforms.SUCCESS
         except VersioningError as e:
-            form.send_message(unicode(e), type='error')
+            form.send_message(e.reason(), type='error')
             return silvaforms.FAILURE
 
 
 class PublicationStatusTableForm(silvaforms.SMISubTableForm):
     """ Manage versions.
     """
-    grok.context(silvainterfaces.IVersionedContent)
+    grok.context(IVersionedContent)
     grok.view(Publish)
     grok.order(30)
 
@@ -480,7 +473,6 @@ class PublicationStatusTableForm(silvaforms.SMISubTableForm):
         CopyForEditingAction(identifier='copy'))
 
     def getItems(self):
-        version_manager = silvainterfaces.IVersionManagement(self.context)
-        versions = version_manager.getVersions(False)
+        versions = IPublicationWorkflow(self.context).get_versions(False)
         versions.sort(sort_versions)
         return [IPublicationStatusInfo(v) for v in versions]
