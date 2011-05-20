6acf893b134255712157cde6610e7c58ee4b56aa
Sylvain Viollon <thefunny@gmail.com>
2011-05-20 16:29:29 +0200

    Move tests from Products.Silva here.

--- src/silva/core/smi/tests/test_func_addables.py
+++ src/silva/core/smi/tests/test_func_addables.py
@@ -1,106 +1,169 @@
+# Copyright (c) 2008-2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
 import unittest
 from Products.Silva.testing import FunctionalLayer, smi_settings
 
 
-class TestAddablesForm(unittest.TestCase):
+class AuthorAddablesTestCase(unittest.TestCase):
+    """An author doesn't have any setting tab nor addable one.
+    """
     layer = FunctionalLayer
+    username = 'author'
 
     def setUp(self):
-        root = self.layer.get_application()
-        factory = root.manage_addProduct['Silva']
+        self.root = self.layer.get_application()
+        self.layer.login('manager')
+        factory = self.root.manage_addProduct['Silva']
         factory.manage_addFolder('folder', 'Folder')
-        self.target = root.folder
-        self.browser = self.layer.get_browser(smi_settings)
 
-    def test_addables_editor(self):
-        self.browser.login('editor')
-        status = self.browser.open('http://localhost/root/edit/tab_addables')
-        self.assertEquals(401, status)
+    def test_addables(self):
+        browser = self.layer.get_selenium_browser(smi_settings)
+        browser.login(self.username)
 
-    def test_addables_tab(self):
-        self.browser.login('chiefeditor')
-        status = self.browser.open('http://localhost/root/edit/tab_addables')
-        self.assertEquals(200, status)
-        self.assertEquals([], self.browser.inspect.feedback)
+        browser.open('/root/edit')
+        self.assertFalse('settings' in browser.inspect.content_tabs)
 
-    def test_addables_still_acquired(self):
-        self.browser.login('chiefeditor')
-        self.assertTrue(self.target.is_silva_addables_acquired())
-        status = self.browser.open(
-            'http://localhost/root/folder/edit/tab_addables')
-        self.assertEquals(200, status)
-        form = self.browser.get_form('form')
-        acquire_field = form.get_control('form.field.acquire')
-        save_button = form.get_control(
-            'form.action.save-addables-settings')
-        self.assertTrue(acquire_field)
-        self.assertTrue(acquire_field.checkable)
-        self.assertTrue(acquire_field.checked)
-        self.assertTrue(save_button)
-        status = save_button.click()
-        self.assertEquals(200, status)
-        self.assertTrue(self.target.is_silva_addables_acquired())
-        self.assertEquals(
-            ['Addable settings have not changed and remain acquired.'],
-            self.browser.inspect.feedback)
 
-    def test_addables_set_not_aquired(self):
-        self.browser.login('chiefeditor')
-        self.assertTrue(self.target.is_silva_addables_acquired())
-        status = self.browser.open(
-            'http://localhost/root/folder/edit/tab_addables')
-        self.assertEquals(200, status)
-        form = self.browser.get_form('form')
-        acquire_field = form.get_control('form.field.acquire')
-        save_button = form.get_control(
-            'form.action.save-addables-settings')
-        acquire_field.checked = False
-        self.assertEquals(200, save_button.click())
-        self.assertFalse(self.target.is_silva_addables_acquired())
-        self.assertEquals(['Changes to addables content types saved.'],
-            self.browser.inspect.feedback)
+class EditorAddablesTestCase(AuthorAddablesTestCase):
+    """An editor have a setting tab but no addable.
+    """
+    username = 'editor'
 
-    def test_now_acquired(self):
-        self.browser.login('chiefeditor')
-        self.target.set_silva_addables_allowed_in_container('Silva Document')
-        self.assertFalse(self.target.is_silva_addables_acquired())
-        status = self.browser.open(
-            'http://localhost/root/folder/edit/tab_addables')
-        self.assertEquals(200, status)
-        form = self.browser.get_form('form')
-        acquire_field = form.get_control('form.field.acquire')
-        save_button = form.get_control(
-            'form.action.save-addables-settings')
-        self.assertFalse(acquire_field.checked)
-        acquire_field.checked = True
-        self.assertEquals(200, save_button.click())
-        self.assertTrue(self.target.is_silva_addables_acquired())
-        self.assertEquals(['Addable settings are now aquired.'],
-            self.browser.inspect.feedback)
+    def test_addables(self):
+        browser = self.layer.get_selenium_browser(smi_settings)
+        browser.login(self.username)
 
-    def test_change_addables(self):
-        self.browser.login('chiefeditor')
-        status = self.browser.open(
-            'http://localhost/root/folder/edit/tab_addables')
-        self.assertEquals(200, status)
-        form = self.browser.get_form('form')
-        acquire_field = form.get_control('form.field.acquire')
-        save_button = form.get_control(
-            'form.action.save-addables-settings')
-        acquire_field.checked = False
-        addables_field = form.get_control('form.field.addables')
-        self.assertTrue('Silva Link' in addables_field.value)
-        new_value = list(addables_field.value)
-        new_value.remove('Silva Link')
-        addables_field.value = new_value
-        self.assertEquals(200, save_button.click())
-        self.assertTrue('Silva Link' not in
-            self.target.get_silva_addables_allowed_in_container())
-        self.assertEquals(['Changes to addables content types saved.'],
-            self.browser.inspect.feedback)
+        browser.open('/root/edit')
+        self.assertTrue('settings' in browser.inspect.content_tabs)
+
+        browser.inspect.content_tabs['settings'].click()
+        self.assertFalse('addables' in browser.inspect.content_subtabs)
+
+
+class ChiefEditorAddablesTestCase(EditorAddablesTestCase):
+    """ChiefEditor (and Manager) have a setting tab and a addable one.
+    """
+    username = 'chiefeditor'
+
+    def test_addables(self):
+        browser = self.layer.get_selenium_browser(smi_settings)
+        browser.login(self.username)
+
+        browser.open('/root/edit')
+        self.assertTrue('settings' in browser.inspect.content_tabs)
+
+        browser.inspect.content_tabs['settings'].click()
+        self.assertTrue('addables' in browser.inspect.content_subtabs)
+
+        browser.inspect.content_subtabs['addables'].click()
+
+        # Addables form
+        form = browser.get_form('form')
+        addables = form.get_control('form.field.addables')
+        self.assertTrue('Silva File' in addables.options)
+        self.assertTrue('Silva Image' in addables.options)
+        self.assertTrue('Silva Folder' in addables.options)
+        self.assertTrue('Silva Link' in addables.options)
+        self.assertTrue('Silva AutoTOC' in addables.options)
+        self.assertTrue('Silva Publication' in addables.options)
+
+        # Uninstalled products are not addables
+        self.assertFalse('Silva Find' in addables.options)
+
+        # Change the addables
+        addables.value = ['Silva Folder', 'Silva File', 'Silva Image']
+        browser.inspect.form_controls['save'].click()
+
+        self.assertItemsEqual(
+            browser.inspect.feedback,
+            ['Changes to addables content types saved.'])
+
+        form = browser.get_form('form')
+        self.assertItemsEqual(
+            form.get_control('form.field.addables').value,
+            ['Silva Folder', 'Silva File', 'Silva Image'])
+
+        # There is no acquire option on the root.
+        self.assertTrue('form.field.acquire' not in form.controls)
+
+        # Now check the add menu: only the three selected are there
+        self.assertTrue('add' in browser.inspect.content_tabs)
+
+        browser.inspect.content_tabs['add'].click()
+        self.assertItemsEqual(
+            browser.inspect.content_subtabs.keys(),
+            ['Silva Folder', 'Silva File', 'Silva Image'])
+
+        # Visit addable tab on this folder.
+        self.assertTrue('content' in browser.inspect.content_tabs)
+        browser.inspect.content_tabs['content'].click()
+
+        # We have one folder, with an addable tab as well
+        self.assertEqual(browser.inspect.folder_identifier, ['folder'])
+        browser.inspect.folder_goto[0].click()
+
+        # And we can only add acquired values
+        self.assertTrue('add' in browser.inspect.content_tabs)
+
+        browser.inspect.content_tabs['add'].click()
+        self.assertItemsEqual(
+            browser.inspect.content_subtabs.keys(),
+            ['Silva Folder', 'Silva File', 'Silva Image'])
+
+        self.assertTrue('settings' in browser.inspect.content_tabs)
+
+        browser.inspect.content_tabs['settings'].click()
+        self.assertTrue('addables' in browser.inspect.content_subtabs)
+
+        browser.inspect.content_subtabs['addables'].click()
+
+        # There is a form, where settings are acquired.
+        form = browser.get_form('form')
+        addables = form.get_control('form.field.addables')
+        self.assertEqual(
+            form.get_control('form.field.acquire').checked,
+            True)
+        self.assertItemsEqual(
+            form.get_control('form.field.addables').value,
+            ['Silva Folder', 'Silva File', 'Silva Image'])
+
+        # Now change the values
+        form.get_control('form.field.acquire').checked = False
+        form.get_control('form.field.addables').value = [
+            'Silva Folder', 'Silva Link', 'Silva AutoTOC']
+        browser.inspect.form_controls['save'].click()
+
+        self.assertItemsEqual(
+            browser.inspect.feedback,
+            ['Changes to addables content types saved.'])
+
+        form = browser.get_form('form')
+        self.assertItemsEqual(
+            form.get_control('form.field.addables').value,
+            ['Silva Folder', 'Silva Link', 'Silva AutoTOC'])
+        self.assertEqual(
+            form.get_control('form.field.acquire').checked,
+            False)
+
+        # Entries in the add menu changed
+        self.assertTrue('add' in browser.inspect.content_tabs)
+
+        browser.inspect.content_tabs['add'].click()
+        self.assertItemsEqual(
+            browser.inspect.content_subtabs.keys(),
+            ['Silva Folder', 'Silva Link', 'Silva AutoTOC'])
+
+
+class ManagerAddablesTestCase(ChiefEditorAddablesTestCase):
+    username = 'manager'
 
 
 def test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestAddablesForm))
+    suite.addTest(unittest.makeSuite(AuthorAddablesTestCase))
+    suite.addTest(unittest.makeSuite(EditorAddablesTestCase))
+    suite.addTest(unittest.makeSuite(ChiefEditorAddablesTestCase))
+    suite.addTest(unittest.makeSuite(ManagerAddablesTestCase))
     return suite
---
+++ src/silva/core/smi/tests/test_func_feeds.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2008-2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+import unittest
+
+from Products.Silva.testing import FunctionalLayer, smi_settings
+
+
+class FeedsTestCase(unittest.TestCase):
+    """Test properties tab of a Folder.
+    """
+    layer = FunctionalLayer
+
+    def setUp(self):
+        self.root = self.layer.get_application()
+        self.layer.login('manager')
+        factory = self.root.manage_addProduct['Silva']
+        factory.manage_addFolder('folder', 'Test Folder')
+
+    def test_feeds(self):
+        xml_browser = self.layer.get_browser()
+        # By default feeds are off
+        self.assertEqual(xml_browser.open('/root/folder/atom.xml'), 404)
+        self.assertEqual(xml_browser.open('/root/folder/rss.xml'), 404)
+
+        # We can enable it in settings
+        browser = self.layer.get_selenium_browser(smi_settings)
+        browser.login('manager')
+        browser.open('/root/folder/edit')
+
+        self.assertTrue('settings' in browser.inspect.content_tabs)
+        browser.inspect.content_tabs['settings'].click()
+        self.assertTrue('settings' in browser.inspect.content_subtabs)
+        browser.inspect.content_subtabs['settings'].click()
+
+        form = browser.get_form('form.feedsform')
+        allow = form.get_control('form.feedsform.field.allow')
+        self.assertEqual(allow.checked, False)
+        allow.checked = True
+        self.assertTrue('Change feed settings' in browser.inspect.form_controls)
+        browser.inspect.form_controls['change feed settings'].click()
+        self.assertEqual(browser.inspect.feedback, ['Feed settings saved.'])
+
+        # Now feeds works
+        self.assertEqual(xml_browser.open('/root/folder/atom.xml'), 200)
+        self.assertEqual(xml_browser.content_type, 'text/xml;charset=UTF-8')
+        self.assertEqual(xml_browser.open('/root/folder/rss.xml'), 200)
+        self.assertEqual(xml_browser.content_type, 'text/xml;charset=UTF-8')
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(FeedsTestCase))
+    return suite
