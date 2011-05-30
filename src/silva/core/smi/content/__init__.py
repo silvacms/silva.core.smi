593ddd947b9a7273c79755cc17310a68e097823b
Sylvain Viollon <thefunny@gmail.com>
2011-05-30 16:07:12 +0200

    Change how the edit tab is marked.

--- src/silva/core/smi/content/__init__.py
+++ src/silva/core/smi/content/__init__.py
@@ -5,25 +5,35 @@
 
 from five import grok
 
-from silva.core.interfaces import IContent
-from silva.core.interfaces import INonPublishable
+from silva.core.interfaces import IContent, INonPublishable
 from silva.translations import translate as _
 from silva.ui.menu import MenuItem, ContentMenu
 from zeam.form import silva as silvaforms
+from zope.interface import Interface, classImplements
 
 
-class EditMenu(MenuItem):
+class IEditTab(Interface):
+    """Marker used to mark edit tab.
+    """
+
+
+class ContentEditMenu(MenuItem):
     grok.adapts(ContentMenu, IContent)
     grok.order(10)
     name = _('Edit')
-    screen = silvaforms.SMIEditForm
+    screen = 'content'
+    interface = IEditTab
 
 
-class EditAssetMenu(MenuItem):
+class NonPublishableEditMenu(MenuItem):
     grok.adapts(ContentMenu, INonPublishable)
     grok.order(10)
     name = _(u'Edit')
-    screen = silvaforms.SMIEditForm
+    screen = 'content'
+    interface = IEditTab
+
+
+classImplements(silvaforms.SMIEditForm, IEditTab)
 
 
 
