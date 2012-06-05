a2f322c2bc6a54feb38e46622f27712d8006aed1
Sylvain Viollon <thefunny@gmail.com>
2012-06-05 13:56:30 +0200

    Update menu.

--- src/silva/core/smi/content/__init__.py
+++ src/silva/core/smi/content/__init__.py
@@ -8,10 +8,10 @@ from zope.interface import classImplements
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 from silva.core import conf as silvaconf
-from silva.ui.interfaces import IUIScreen
 from silva.core.interfaces import IContent, INonPublishable
 from silva.translations import translate as _
-from silva.ui.menu import ExpendableMenuItem, MenuItem, ContentMenu
+from silva.ui.interfaces import IUIScreen
+from silva.ui.menu import ExpendableMenuItem, ContentMenu
 from zeam.form import silva as silvaforms
 
 
@@ -19,21 +19,18 @@ class IEditScreen(IUIScreen):
     """Marker used to mark edit tab.
     """
 
+# We cannot do it with one unique class, because of Container
 
-class ContentEditMenu(ExpendableMenuItem):
-    grok.adapts(ContentMenu, IContent)
+class EditMenu(ExpendableMenuItem):
+    grok.adapts(ContentMenu, INonPublishable)
     grok.order(10)
     name = _('Edit')
     screen = 'content'
     interface = IEditScreen
 
 
-class NonPublishableEditMenu(MenuItem):
-    grok.adapts(ContentMenu, INonPublishable)
-    grok.order(10)
-    name = _(u'Edit')
-    screen = 'content'
-    interface = IEditScreen
+class ContentEditMenu(EditMenu):
+    grok.adapts(ContentMenu, IContent)
 
 
 classImplements(silvaforms.SMIEditForm, IEditScreen)
